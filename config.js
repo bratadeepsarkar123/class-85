fa99df6dec70f3541b9923b705e2f35db6b51848
bratadeepsarkar123 <79410839+bratadeepsarkar123@users.noreply.github.com>
2022-03-27 16:36:42 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,13 @@
+import firebase from 'firebase';
+import 'firebase/auth'
+import 'firebase/database'
+import 'firebase/app'
+
+ export const firebaseConfig = {
+  apiKey: "AIzaSyBxuskQvDjjOcdRuQHgJXcZAiuBmcYUwxU",
+  authDomain: "storytelling-app-24bfa.firebaseapp.com",
+  projectId: "storytelling-app-24bfa",
+  storageBucket: "storytelling-app-24bfa.appspot.com",
+  messagingSenderId: "1098934757643",
+  appId: "1:1098934757643:web:dfed1bee62b2af69fe5a0e"
+};
